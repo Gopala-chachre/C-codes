4b7b8fd11b1e48eaaa43ff162c7252eda6d1a0e2
Gopala chachre <chachregopala@gmail.com>
2025-10-17 16:57:20 +0530

    learn getChar & putChar

---
+++ getPutChar.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+
+void main (){ 
+
+    char ch; 
+
+    printf("Enter a Character:"); 
+    ch = getchar(); //getting the Character 
+
+    printf("Your enter character is :"); 
+    putchar( ch );  // printing the Character 
+} 
