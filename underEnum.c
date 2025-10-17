fa2c199005ab6a7b317f7b5b4c9d9d0d8c254328
Gopala chachre <chachregopala@gmail.com>
2025-10-17 19:20:31 +0530

    understanding the Enum

---
+++ underEnum.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+
+      enum Weekdays { 
+      Sunday,     
+      Monday,     
+      Tuesday,    
+      Wednesday,  
+      Thursday,   
+      Friday,     
+      Saturday    
+}; 
+
+void main(){
+    printf("Value of Sunday: %d\n", Sunday); 
+    printf("Value of Monday: %d\n", Monday); 
+    printf("Value of Tuesday: %d\n", Tuesday); 
+    printf("Value of Wednesday: %d\n", Wednesday); 
+    printf("Value of Thursday: %d\n", Thursday); 
+    printf("Value of Friday: %d\n", Friday); 
+    printf("Value of Saturday: %d\n", Saturday); 
+} 
